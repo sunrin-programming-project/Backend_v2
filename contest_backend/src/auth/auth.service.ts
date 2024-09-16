507153f65f2a040f909a202bc4fd45a1af022371
yeongjae.nam <op@ye0ngjae.com>
2024-09-16 19:19:35 +0900

    Fix: User error

--- contest_backend/src/auth/auth.service.ts
+++ contest_backend/src/auth/auth.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    co
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtServiceL: JwtService
+    ) {}
+
+    async getJWT(googleId: string, email: string, name: string) {
+        const payload = { google_id: googleId, email: email, name: name }
+        return this.jwtServiceL.sign(payload)
+    }
+
+    async googleValidate(googleId: string, email: string, name: string) {
+        const user = await this.userService.findUserByGoogleId(googleId)
+        if (!user) {
+            await this.userService.createUser({ googleId, email, name })
+        }
+        return this.getJWT(googleId, email, name)
+    }
 }
