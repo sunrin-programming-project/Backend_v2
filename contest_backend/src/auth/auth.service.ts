0b4e543d2e55d3fa166cee173a5bfe3da280bfa9
yeongjae.nam <op@ye0ngjae.com>
2024-09-18 14:06:26 +0900

    Add: JWT Token generate

--- contest_backend/src/auth/auth.service.ts
+++ contest_backend/src/auth/auth.service.ts
@@ -16,9 +16,16 @@ export class AuthService {
 
     async googleValidate(googleId: string, email: string, name: string) {
         const user = await this.userService.findUserByGoogleId(googleId)
+
         if (!user) {
-            await this.userService.createUser({ googleId, email, name })
+            await this.userService.createUser(googleId, email, name)
         }
+
         return this.getJWT(googleId, email, name)
     }
+
+    async generateRefreshToken(googleId: string, email: string, name: string) {
+        return this.getJWT(googleId, email, name)
+    }
+
 }
